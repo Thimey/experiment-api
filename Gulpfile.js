48ad695ad28f80aef26874b39bdc0f500eea5df3
simon Angell <simonangell@simons-mbp.ait-sydney.net.au>
2016-07-01 11:31:21 +1000

    added test watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var sync    = require('run-sequence');
 
+var paths = {
+	testIntegration: 'server/api/**/*.integration.js',
+	api: ['server/api/**/*.js', 'server/auth/**/*.js']
+};
+
+// Tests
 gulp.task('test:integration', function () {
-	return gulp.src(['server/api/**/*.integration.js'], {read: false})
+	return gulp.src([paths.testIntegration], {read: false})
 		.pipe(mocha({
       reporter: 'spec',
       globals: {
@@ -12,4 +19,9 @@ gulp.task('test:integration', function () {
     }));
 });
 
-gulp.task('test', ['test:integration']);
+// Watch
+gulp.task('watch-test', function () {
+	gulp.watch(paths.api, ['test:integration']);
+});
+
+gulp.task('test', ['test:integration', 'watch-test']);
--- server/api/user/user.integration.js
+++ server/api/user/user.integration.js
@@ -6,31 +6,33 @@ var mongoose = require('mongoose');
 var User = require('./user.model.js');
 
 describe('users', function () {
+	var userCount;
 
 	before(function (done) {
-		// mongoose.connection.db.dropDatabase(function () {
-		// 	console.log('dropping db');
-		// 	done();
-		// });
-		function clearDB() {
-	    var promises = [
-	        User.remove().exec(),
-	    ];
+    console.log('before');
+    var promises = [
+       User.remove().exec()
+    ];
 
-	    Promise.all(promises)
-	        .then(function () {
-	            done();
-	        })
-    }
+    Promise.all(promises)
+	    .then(function () {
+	      done();
+	    });
+	});
 
-    clearDB();
+	beforeEach(function (done) {
+		User.count({}, function (err, count) {
+			userCount = count;
+			console.log(count);
+			done();
+		});
 	});
 
 	describe('creating a user', function () {
 		
 		it('should create a user on signup', function (done) {
 			var user = {
-				"email": "test@example4.com",
+				"email": "test@example.com",
 				"password": "password"
 			};
 			
@@ -41,10 +43,20 @@ describe('users', function () {
 				.expect('Content-Type', /json/)
       	.expect(200)
       	.end(function (err, res) {
-      		expect(res.body.user.email).to.equal('test@example4.com');
-      		done();
+      		expect(res.body.user.email).to.equal('test@example.com');
+      		User.count({}, function (err, count) {
+      			expect(count).to.equal(userCount + 1);
+      			done();
+      		});
       	});
 		});
+
+		// it('delete a user', function (done) {
+
+		// 	request(app)
+		// 		.delete('/api/user')
+		// 		.
+		// });
 
 	});
 	
